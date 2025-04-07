96df12cc87e569b2b48bb9e9775c76a39f59983f
leverindev <leverin@gmail.com>
2025-04-07 09:43:53 +0300

    Add monads: Option, Result

---
+++ src/MonadsExtensions/OptionContainer/Option.cs
@@ -0,0 +1,28 @@
+﻿namespace MonadsExtensions.OptionContainer
+{
+    public readonly struct Option<T>
+    {
+        private readonly T _value;
+
+        private Option(T value, bool hasValue)
+        {
+            _value = value;
+            HasValue = hasValue;
+        }
+
+        public Option(T value) : this(value, true)
+        {
+        }
+
+        public T Value => HasValue ? _value : default;
+
+        public bool HasValue { get; }
+
+        public bool TryGetValue(out T value)
+        {
+            value = Value;
+
+            return HasValue;
+        }
+    }
+}
---
+++ src/MonadsExtensions/ResultContainer/Result.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MonadsExtensions.ResultContainer
+{
+    public readonly struct Result<TValue, TError>
+    {
+        public Result(TValue value, TError error, bool hasValue)
+        {
+            Value = value;
+            Error = error;
+            HasValue = hasValue;
+        }
+
+        public Result(TValue value) : this(value, default, true)
+        {
+        }
+
+        public Result(TError error) : this(default, error, false)
+        {
+        }
+
+        public TValue Value { get; }
+
+        public TError Error { get; }
+
+        public bool HasValue { get; }
+
+        public void Do(Action<TValue> onSuccess, Action<TError> onError)
+        {
+            if (HasValue)
+            {
+                onSuccess(Value);
+            }
+            else
+            {
+                onError(Error);
+            }
+        }
+
+        public T Bind<T>(Func<TValue, T> onSuccess, Func<TError, T> onError)
+        {
+            if (HasValue)
+            {
+                return onSuccess != null ? onSuccess(Value) : default;
+            }
+
+            return onError != null ? onError(Error) : default;
+        }
+
+        public void Deconstruct(out TValue value, out TError error)
+        {
+            value = Value;
+            error = Error;
+        }
+
+        public bool IsValue(out TValue value, out TError error)
+        {
+            value = default;
+            error = default;
+
+            if (HasValue)
+            {
+                value = Value;
+            }
+            else
+            {
+                error = Error;
+            }
+
+            return HasValue;
+        }
+
+        public bool IsError(out TValue value, out TError error) => !IsValue(out value, out error);
+    }
+}
