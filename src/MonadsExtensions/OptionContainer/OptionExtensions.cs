7b56a8dd9f1c5efbeef8b61dfbc88f8910606869
leverindev <leverin@gmail.com>
2025-04-07 09:55:44 +0300

    Option: add Unwrap extension methods

--- src/MonadsExtensions/OptionContainer/OptionExtensions.cs
+++ src/MonadsExtensions/OptionContainer/OptionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MonadsExtensions.OptionContainer.Models;
+﻿using System;
+using MonadsExtensions.OptionContainer.Models;
 
 namespace MonadsExtensions.OptionContainer
 {
@@ -9,5 +10,26 @@ namespace MonadsExtensions.OptionContainer
         public static None None { get; } = new None();
 
         public static Option<T> ToOption<T>(this T value) => Some(value);
+
+        public static T UnwrapOrDefault<T>(this Option<T> result)
+        {
+            return result.HasValue ? result.Value : default;
+        }
+
+        public static T UnwrapOrException<T>(this Option<T> result, Exception exception)
+        {
+            if (result.HasValue)
+            {
+                return result.Value;
+            }
+
+            throw exception;
+        }
+
+        public static T UnwrapOrException<T>(this Option<T> result, string message) =>
+            result.UnwrapOrException(new Exception(message));
+
+        public static TResult UnwrapOrException<TResult>(this Option<TResult> result) =>
+            result.UnwrapOrException(new ArgumentNullException(nameof(result)));
     }
 }
