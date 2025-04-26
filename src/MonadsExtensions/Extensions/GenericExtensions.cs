47604eaaa2569899c2c87808d9d78f738510a02c
leverindev <leverin@gmail.com>
2025-04-26 14:06:06 +0300

    Add async extensions

--- src/MonadsExtensions/Extensions/GenericExtensions.cs
+++ src/MonadsExtensions/Extensions/GenericExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace MonadsExtensions.Extensions
 {
@@ -6,15 +7,88 @@ namespace MonadsExtensions.Extensions
     {
         public static TResult Map<TSource, TResult>(this TSource source, Func<TSource, TResult> func)
         {
-            return func is null ? default : func(source);
+            if (func is null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return func(source);
+        }
+
+        public static Task<TResult> MapAsync<TSource, TResult>(this Task<TSource> sourceTask, Func<TSource, TResult> func)
+        {
+            if (func is null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return sourceTask?.ContinueWith(task => task.Result.Map(func));
+        }
+
+        public static Task<TResult> MapAsync<TSource, TResult>(this Task<TSource> sourceTask, Func<Task<TSource>, TResult> func)
+        {
+            if (func is null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return sourceTask?.ContinueWith(func);
         }
 
         public static T Do<T>(this T obj, Action<T> action)
         {
-            if (obj != null)
+            if (action is null)
             {
-                action(obj);
+                throw new ArgumentNullException(nameof(action));
             }
+
+            if (obj == null)
+            {
+                return default;
+            }
+
+            action(obj);
+
+            return obj;
+        }
+
+        public static Task<T> DoAsync<T>(this Task<T> task, Action<T> action)
+        {
+            return task?.ContinueWith(t => t.Result.Do(action));
+        }
+
+        public static async Task<T> DoAsync<T>(this Task<T> task, Func<Task<T>, Task> action)
+        {
+            if (task == null)
+            {
+                return default;
+            }
+
+            if (action is null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            await action(task);
+
+            return await task;
+        }
+
+        public static async Task<T> DoAsync<T>(this Task<T> task, Func<T, Task> action)
+        {
+            if (task == null)
+            {
+                return default;
+            }
+
+            if (action is null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var obj = await task;
+
+            await action(obj);
 
             return obj;
         }
---
+++ tests/MonadsExtensions.Tests/ResultContainer/SequentialResultAsyncProcessingTests.cs
@@ -0,0 +1,105 @@
+﻿using MonadsExtensions.ResultContainer;
+using MonadsExtensions.Extensions;
+
+namespace MonadsExtensions.Tests.ResultContainer;
+
+public class SequentialResultAsyncProcessingTests : ResultTestsBase
+{
+    // Sequential processing of results is a common pattern in functional programming.
+    // If input is not a number - the result should be -1.
+    // If input is a negative number - the result should be -1.
+    // If input is a positive number and even - the result should be divided by 2.
+    // If input is a positive number and odd - the result should be multiplied by 2.
+    [Theory]
+    [InlineData("42", 21)]
+    [InlineData("21", 42)]
+    [InlineData("-5", -1)]
+    [InlineData("not a number", -1)]
+    public async Task ValidInput_Sqr_ResultHasSqrValueAsync(string input, int expectedValue)
+    {
+        // Arrange
+        // Act
+        var result = (await ParseInt32Async(input)
+                .BindAsync(ToIntermediateResult)
+                .BindAsync(ProcessNegativeAsync)
+                .BindAsync(ProcessMod2Async)
+                .DoAsync(LogErrorAsync))
+            .UnwrapOrElse(ToIntermediateResult)
+            .Map(UnwrapIntermediateResult);
+
+        // Assert
+        Assert.Equal(expectedValue, result);
+    }
+
+    protected async Task<Result<int, string>> ParseInt32Async(string input)
+    {
+        await Task.Yield();
+
+        if (int.TryParse(input, out var value))
+        {
+            return Result.Ok(value);
+        }
+
+        return Result.Error("Invalid input");
+    }
+
+    private static async Task<IntermediateResult<int>> ProcessNegativeAsync(IntermediateResult<int> result)
+    {
+        await Task.Yield();
+
+        if (result.Completed)
+        {
+            return result;
+        }
+
+        return result.Value < 0
+            ? new IntermediateResult<int>(-1, true)
+            : new IntermediateResult<int>(result.Value, false);
+    }
+
+    private static async Task<IntermediateResult<int>> ProcessMod2Async(IntermediateResult<int> result)
+    {
+        await Task.Yield();
+
+        if (result.Completed)
+        {
+            return result;
+        }
+
+        return result.Value % 2 == 0 ?
+            new IntermediateResult<int>(result.Value / 2, true) :
+            new IntermediateResult<int>(result.Value * 2, true);
+    }
+
+    private static async Task LogErrorAsync<T>(Result<T, string> result)
+    {
+        await Task.Yield();
+
+        if (!result.HasValue)
+        {
+            Console.WriteLine(result.Error);
+        }
+    }
+
+    private readonly struct IntermediateResult<T>(T value, bool completed)
+    {
+        public T Value { get; } = value;
+
+        public bool Completed { get; } = completed;
+    }
+
+    private static IntermediateResult<int> ToIntermediateResult(int value)
+    {
+        return new IntermediateResult<int>(value, false);
+    }
+
+    private static IntermediateResult<int> ToIntermediateResult(string error)
+    {
+        return new IntermediateResult<int>(-1, true);
+    }
+
+    private static int UnwrapIntermediateResult(IntermediateResult<int> task)
+    {
+        return task.Completed ? task.Value : -1;
+    }
+}
